f1e6042c19e23f357bed2b09498ee488140b136c
shouaya <favaserver@163.com>
2016-12-20 15:55:16 +0900

    add selenium

--- src/main/java/com/jialu/minipage/MiniPage.java
+++ src/main/java/com/jialu/minipage/MiniPage.java
@@ -3,8 +3,19 @@ package com.jialu.minipage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.google.common.io.Resources;
 
 public class MiniPage {
 
@@ -13,18 +24,42 @@ public class MiniPage {
 			System.out.println("Parameters Size Error");
 			throw new RuntimeException();
 		}
-		PageDesignObject obj = readDesignFile(args[2]);
-		Boolean suucess = executeDesignAll(args[0], obj);
-		if (suucess) {
-			System.exit(0);
-		} else {
-			System.exit(1);
-		}
+		PageDesignObject obj = readDesignFile(args[0]);
+		System.setProperty("webdriver.chrome.driver", args[1]);
+		String path = createHtmlFile(obj);
+		previewHtmlFile(path, args[2]);
 	}
 
-	private static Boolean executeDesignAll(String string, PageDesignObject obj) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+	private static void previewHtmlFile(String path, String device) {
+		Map<String, String> mobileEmulation = new HashMap<String, String>();
+		mobileEmulation.put("deviceName", device);
+		Map<String, Object> chromeOptions = new HashMap<String, Object>();
+		chromeOptions.put("mobileEmulation", mobileEmulation);
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+		WebDriver driver = new ChromeDriver(capabilities);
+		driver.get(path);
+	}
+
+	private static String createHtmlFile(PageDesignObject obj) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		String headContent = Resources.toString(Resources.getResource("header.html"),
+				Charset.forName(CharEncoding.UTF_8));
+		sb.append(headContent);
+		String bodyContent = createBodyContent(obj);
+		sb.append(bodyContent);
+		String footContent = Resources.toString(Resources.getResource("footer.html"),
+				Charset.forName(CharEncoding.UTF_8));
+		sb.append(footContent);
+		File file = new File("out/app.html");
+		FileUtils.writeStringToFile(file, sb.toString(), Charset.forName(CharEncoding.UTF_8));
+		return file.getCanonicalPath();
+	}
+
+	private static String createBodyContent(PageDesignObject obj) throws IOException {
+		String content = Resources.toString(Resources.getResource("templates/flex.html"),
+				Charset.forName(CharEncoding.UTF_8));
+		return content;
 	}
 
 	private static PageDesignObject readDesignFile(String file) throws IOException {
@@ -33,7 +68,7 @@ public class MiniPage {
 
 		try {
 			execlFileStream = new FileInputStream(new File(file));
-			HSSFWorkbook workbook = new HSSFWorkbook(execlFileStream);
+			XSSFWorkbook workbook = new XSSFWorkbook(execlFileStream);
 			// object.setSteps(createTestSteps(workbook.getSheetAt(0)));
 			// object.setDatas(createTestDatas(workbook.getSheetAt(1)));
 		} catch (IOException e) {
