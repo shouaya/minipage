0a929b504536404aa25d5aebf42837c03496d809
shouaya <favaserver@163.com>
2017-01-18 15:19:48 +0900

    fix

--- out/app.js
+++ out/app.js
@@ -6,9 +6,7 @@
 // Here is how to define your module
 // has dependent on mobile-angular-ui
 //
-var app = angular.module('MiniProfile', [
-  'ngRoute'
-]);
+var app = angular.module('MiniProfile', ['ngResource', 'ngRoute']);
 
 //
 // You can configure ngRoute as always, but to take advantage of SharedState location
@@ -19,14 +17,29 @@ app.config(function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'profile.html', reloadOnSearch: false});
 });
 
+app.factory('Profile', ['$resource', function($resource) {
+  // 「get」「find」「create」の3つのアクションを定義
+  return $resource(
+    'https://api.9jialu.com/profile/:id',
+    {id: '@id'},
+    {
+      get: {method: 'GET', isArray: true},
+      find: {method: 'GET'},
+      create: {method: 'POST'}
+    }
+  );
+}]);
+
 //
 // For this trivial demo we have just a unique MainController
 // for everything
 //
-app.controller('MiniController', function($rootScope, $scope) {
-
-  $scope.swiped = function(direction) {
-    alert('Swiped ' + direction);
-  };
-
-});
+app.controller('MiniController', ['$rootScope', '$scope', 'Profile',
+  function($rootScope, $scope, Profile) {
+	alert("what");
+	Profile.get({id:1}).$promise.then(function(user) {
+		$scope.user = user;
+	}).catch(function(data, status) {
+	    alert('error');
+  	});
+}]);
