f1fd4d92e7ec39a0178e82cd3177fe3abbba09ba
shouaya <favaserver@163.com>
2017-02-09 18:35:53 +0900

    fix image module

--- out/app.js
+++ out/app.js
@@ -51,3 +51,94 @@ function returnHome(){
 		riot.mount('admin', opts);
 	}
 }
+function ajaxget(url, success){
+	$('#loadingToast').fadeIn(100);
+	$.ajax({
+		url : url,
+		type : 'GET',
+		dataType:"json",
+		contentType : 'application/json',
+		success : success,
+		complete : function(xhr, status){
+			$('#loadingToast').fadeOut(100);
+		},
+		error : function(xhr, type) {
+			msgerror();
+		}
+	});
+}
+function ajaxpost(url, json, callback){
+	$.ajax({
+		url : url,
+		type : 'POST',
+		dataType:"json",
+		contentType : 'application/json',
+		data : JSON.stringify(json),
+		success : function(data, status) {
+			callback(data, status);
+			msgdone();
+		},
+		error : function(xhr, type) {
+			msgerror();
+		}
+	});
+}
+function ajaxupload(url, form, callback){
+	$('#loadingToast').fadeIn(100);
+	$.ajax({
+		url : url,
+		type : 'POST',
+		processData: false,
+		contentType: false,
+		dataType: 'json',
+		data : form,
+		success : function(data, status) {
+			callback(data, status);
+			msgdone();
+		},
+		complete : function(xhr, status){
+			$('#loadingToast').fadeOut(100);
+		},
+		error : function(xhr, type) {
+			msgerror();
+		}
+	});
+}
+function msgdone(){
+	$('#doneToast').fadeIn(100);
+	setTimeout(function () {
+		$('#doneToast').fadeOut(100);
+	}, 2000);
+}
+function msgerror(){
+	$('#errorToast').fadeIn(100);
+	setTimeout(function () {
+		$('#errorToast').fadeOut(100);
+	}, 2000);
+}
+function fileUpload(elem, callback){
+	var file = elem.files[0];
+    name = file.name;
+    size = file.size;
+    type = file.type;
+	var output = [];
+    if(file.name.length < 1) {
+    }
+    else if(file.size > 100000) {
+        //alert("The file is too big");
+		output.push('<i class="remove icon"></i><p><strong>ファイルが大きすぎます</strong> <br/>(', file.type || 'n/a', ') <br/> ', file.size, ' bytes', '<p>');
+    }
+    else if(file.type != 'image/png' && file.type != 'image/jpg' && file.type != 'image/gif' && file.type != 'image/jpeg' ) {
+        //alert("The file does not match png, jpg or gif");
+		output.push('<i class="remove icon"></i><p><strong>イメージのみ</strong><br/> (', file.type || 'n/a', ') <br/> ', file.size, ' bytes', '<p>');
+    }else{
+		var formData = new FormData();
+        formData.append("file", file);
+		$('#loadingToast').fadeIn(100);
+		ajaxupload('/file/upload', formData, function(data, status){
+			callback(data, status);
+		});
+		output.push('<i class="checkmark icon"></i><p><strong>', escape(file.name), '</strong><br/> (', file.type || 'n/a', ') <br/> ', file.size, ' bytes', '<p>');
+	}
+    document.getElementById('list').innerHTML =  output.join('') ;
+}
